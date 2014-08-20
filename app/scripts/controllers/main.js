60b58f147434b2ef1f50173062c06a388fa3f4e3
Federico Rota <federico.rota01@gmail.com>
2014-08-20 22:53:20 +0200

    Integrazione ngDraggable

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,19 @@
 'use strict';
 
-angular.module('emailTemplateBuilderApp')
+angular.module('emailTemplateBuilderApp', ['ngDraggable'])
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+   $scope.draggableObjects = [{name:'one'}, {name:'two'}, {name:'three'}, {name:'four'}];
+        $scope.droppedObjects1 = [];
+        $scope.droppedObjects2= [];
+        $scope.onDropComplete1=function(data,evt){
+            var index = $scope.droppedObjects1.indexOf(data);
+            if (index == -1)
+            $scope.droppedObjects1.push(data);
+        }
+        $scope.onDragSuccess1=function(data,evt){
+            var index = $scope.droppedObjects1.indexOf(data);
+            if (index > -1) {
+                $scope.droppedObjects1.splice(index, 1);
+            }
+        }
   });
