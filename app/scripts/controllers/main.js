14b957271b6593ba8fc7c2ef3987e71ed392bdd1
Federico Rota <federico.rota01@gmail.com>
2014-08-23 11:34:43 +0200

    Reorder

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,19 +1,24 @@
 'use strict';
 
 angular.module('emailTemplateBuilderApp', ['ngDraggable'])
-  .controller('MainCtrl', function ($scope) {
-   $scope.draggableObjects = [{name:'one'}, {name:'two'}, {name:'three'}, {name:'four'}];
-        $scope.droppedObjects1 = [];
-        $scope.droppedObjects2= [];
-        $scope.onDropComplete1=function(data,evt){
-            var index = $scope.droppedObjects1.indexOf(data);
-            if (index == -1)
-            $scope.droppedObjects1.push(data);
-        }
-        $scope.onDragSuccess1=function(data,evt){
-            var index = $scope.droppedObjects1.indexOf(data);
-            if (index > -1) {
-                $scope.droppedObjects1.splice(index, 1);
-            }
-        }
-  });
+	.controller('MainCtrl', function($scope) {
+		$scope.draggableObjects = [{name: 'one'}, {name: 'two'}, {name: 'three'}, {name: 'four'}]; 
+		$scope.droppedObjects = []; 
+
+		$scope.onDropComplete = function(data, evt) {
+			var index = $scope.droppedObjects.indexOf(data);
+			if (index == -1)
+				$scope.droppedObjects.push(data);
+			
+            var otherObj = $scope.draggableObjects[index];
+            var otherIndex = $scope.draggableObjects.indexOf(obj);
+            $scope.draggableObjects[index] = obj;
+            $scope.draggableObjects[otherIndex] = otherObj;
+		}
+		$scope.onDragSuccess = function(data, evt) {
+			var index = $scope.droppedObjects.indexOf(data);
+			if (index > -1) {
+				$scope.droppedObjects.splice(index, 1);
+			}
+		}
+	});
