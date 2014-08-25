67b6ee0777f50403bcaf2e32d136ddeb56ee7454
Federico Rota <federico.rota01@gmail.com>
2014-08-25 19:20:33 +0200

    Clone + reorder, layout da fixare

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,23 +2,30 @@
 
 angular.module('emailTemplateBuilderApp', ['ngDraggable'])
 	.controller('MainCtrl', function($scope) {
-		$scope.draggableObjects = [{name: 'one'}, {name: 'two'}, {name: 'three'}, {name: 'four'}]; 
-		$scope.droppedObjects = []; 
 
-		$scope.onDropComplete = function(data, evt) {
-			var index = $scope.droppedObjects.indexOf(data);
-			if (index == -1)
-				$scope.droppedObjects.push(data);
-			
-            var otherObj = $scope.draggableObjects[index];
-            var otherIndex = $scope.draggableObjects.indexOf(obj);
-            $scope.draggableObjects[index] = obj;
-            $scope.draggableObjects[otherIndex] = otherObj;
-		}
-		$scope.onDragSuccess = function(data, evt) {
-			var index = $scope.droppedObjects.indexOf(data);
-			if (index > -1) {
-				$scope.droppedObjects.splice(index, 1);
-			}
-		}
+        $scope.draggableObjects = [{name:'one'}, {name:'two'}, {name:'three'}];
+        $scope.droppedObjects1 = [];
+        $scope.droppedObjects2= [];
+        $scope.onDropComplete1=function(data,evt){
+            var index = $scope.droppedObjects1.indexOf(data);
+            if (index == -1)
+            $scope.droppedObjects1.push(data);
+        }
+        $scope.onDragSuccess1=function(data,evt){
+            // var index = $scope.droppedObjects1.indexOf(data);
+            // if (index > -1) {
+            //     $scope.droppedObjects1.splice(index, 1);
+            // }
+        }
+        
+        $scope.onDropComplete = function (index, obj, evt) {
+            var otherObj = $scope.droppedObjects1[index];
+            var otherIndex = $scope.droppedObjects1.indexOf(obj);
+            $scope.droppedObjects1[index] = obj;
+            $scope.droppedObjects1[otherIndex] = otherObj;
+        }
+
+        var inArray = function(array, obj) {
+            var index = array.indexOf(obj);
+        }
 	});
