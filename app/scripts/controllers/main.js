f408bff45fbf5c5fa246d7f51f081c21b8159f40
Federico Rota <federico.rota01@gmail.com>
2014-08-25 23:25:53 +0200

    Sanitize html

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,52 +1,52 @@
 'use strict';
 
-angular.module('emailTemplateBuilderApp', ['ngDraggable'])
-	.controller('MainCtrl', function($scope) {
+angular.module('emailTemplateBuilderApp', ['ngSanitize','ngDraggable'])
+	.controller('MainCtrl', function($scope, $sce) {
 
-        $scope.draggableObjects = [
-        {
-        	contenuto:'Lorem ipsum <strong>dolor</strong> sit amet...', 
-        	type: 'testo'
-        }, 
-        {
-        	contenuto:'two', 
-        	type: 'immagine'
-        }, 
-        {
-        	contenuto:'three', 
-        	type: 'spacer'
-        }, 
-        {
-        	contenuto:'four', 
-        	type: 'divider'
-        }, 
-        {
-        	contenuto:'five', 
-        	type: 'button'
-        }
-        ];
-        $scope.droppedObjects = [];
+		$scope.draggableObjects = [{
+			contenuto: 'Lorem ipsum <strong>dolor</strong> sit amet...',
+			type: 'testo'
+		}, {
+			contenuto: 'two',
+			type: 'immagine'
+		}, {
+			contenuto: 'three',
+			type: 'spacer'
+		}, {
+			contenuto: 'four',
+			type: 'divider'
+		}, {
+			contenuto: 'five',
+			type: 'button'
+		}];
+		$scope.droppedObjects = [];
 
-        $scope.onDropComplete=function(data,evt){
-            var index = $scope.droppedObjects.indexOf(data);
-            if (index == -1)
-            $scope.droppedObjects.push(data);
-        }
-        $scope.onDragSuccess=function(data,evt){
-            // var index = $scope.droppedObjects.indexOf(data);
-            // if (index > -1) {
-            //     $scope.droppedObjects.splice(index, 1);
-            // }
-        }
+		$scope.onDropComplete = function(data, evt) {
+			var index = $scope.droppedObjects.indexOf(data);
+			if (index == -1)
+				$scope.droppedObjects.push(data);
+		}
+		$scope.onDragSuccess = function(data, evt) {
+			// var index = $scope.droppedObjects.indexOf(data);
+			// if (index > -1) {
+			//     $scope.droppedObjects.splice(index, 1);
+			// }
+		}
 
-        $scope.onDropCompleteReorder = function (index, obj, evt) {
-            var otherObj = $scope.droppedObjects[index];
-            var otherIndex = $scope.droppedObjects.indexOf(obj);
-            $scope.droppedObjects[index] = obj;
-            $scope.droppedObjects[otherIndex] = otherObj;
-        }
+		$scope.onDropCompleteReorder = function(index, obj, evt) {
+			var otherObj = $scope.droppedObjects[index];
+			var otherIndex = $scope.droppedObjects.indexOf(obj);
+			$scope.droppedObjects[index] = obj;
+			$scope.droppedObjects[otherIndex] = otherObj;
+		}
 
-        var inArray = function(array, obj) {
-            var index = array.indexOf(obj);
-        }
+		$scope.removeComponent = function(obj) {
+			var index = $scope.droppedObjects.indexOf(obj);
+			if (index > -1) {
+				$scope.draggableObjects.splice(index, 1);
+			}
+		}
+		var inArray = function(array, obj) {
+			var index = array.indexOf(obj);
+		}
 	});
