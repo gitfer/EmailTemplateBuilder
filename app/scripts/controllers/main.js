e44065cd5a85600a75442dd1014e0d2d7f342e5a
Federico Rota <federico.rota01@gmail.com>
2014-09-04 22:20:29 +0200

    Drop area, non ancora funzionante

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('emailTemplateBuilderApp', ['ngSanitize', 'ngDraggable'])
-	.controller('MainCtrl', function($scope, $sce) {
+	.controller('MainCtrl', function($scope, $sce, $filter) {
 
 		$scope.draggableObjects = [{
 			contenuto: 'Lorem ipsum <strong>dolor</strong> sit amet...',
@@ -12,7 +12,7 @@ angular.module('emailTemplateBuilderApp', ['ngSanitize', 'ngDraggable'])
 			type: 'testo',
 			allineamento: 'inline'
 		},{
-			contenuto: '<div style="display:block !important; clear:both; width:100%;"><p style="height: 60px"></p></div>',
+			contenuto: '<div style="display:block !important; clear:both; width:100%;"><div style="height: 60px"><br /><br /></div></div>',
 			type: 'spacer',
 			allineamento: 'block'
 		},
@@ -38,20 +38,28 @@ angular.module('emailTemplateBuilderApp', ['ngSanitize', 'ngDraggable'])
 				$scope.droppedObjects.push(clone(data));
 			}
 		}
+		//TODO: refactoring
+		Array.prototype.max = function (property, max_value) {
+				
+				if(this.length === 0)
+					return 0;
+
+				var max_value = max_value || 1;
+			    for (var i = 0, len = this.length; i < len; i++) {
+			        if (this[i][property] >= max_value)
+			        	max_value = this[i][property]
+			    }
+			    return max_value;
+    	};
+
 		var clone = function  (data) {
 				var el = {};
-				for(var proprieta in data){
-					el[proprieta] = data[proprieta]; 
-				}
-				el.id = $scope.droppedObjects.length + 1;
+				angular.copy(data, el);
+				el.id = $scope.droppedObjects.max('id', 1) + 1;
 				return el;
 		}
 		$scope.onDragSuccess = function(data, evt) {
-			// console.log('onDragSuccess', data, evt)
-			// var index = $scope.droppedObjects.indexOf(data);
-			// if (index > -1) {
-			// 	$scope.droppedObjects.splice(index, 1);
-			// }
+			console.log(' $scope.droppedObjects:onDragSuccess',  $scope.droppedObjects.length)
 		}
 
 		$scope.onDropCompleteReorder = function(index, obj, evt) {
@@ -64,12 +72,12 @@ angular.module('emailTemplateBuilderApp', ['ngSanitize', 'ngDraggable'])
 		}
 
 		$scope.removeComponent = function(obj) {
+		
 			var index = $scope.droppedObjects.indexOf(obj);
+			console.log(index)
 			if (index > -1) {
-				$scope.draggableObjects.splice(index, 1);
+			console.log(' $scope.droppedObjects:removeComponent',  $scope.droppedObjects)
+				$scope.droppedObjects.splice(index, 1);
 			}
-		}
-		var inArray = function(array, obj) {
-			var index = array.indexOf(obj);
 		}
 	});
